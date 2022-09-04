aea7c76306f3c9bd8df92e6e2c86203ace3bc6af
mkamakura <kamakura.masaya@gmail.com>
2022-09-04 15:19:37 +0900

    create themes

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -1,28 +1,51 @@
-// In this file, all Page components from 'src/pages` are auto-imported. Nested
-// directories are supported, and should be uppercase. Each subdirectory will be
-// prepended onto the component name.
-//
-// Examples:
-//
-// 'src/pages/HomePage/HomePage.js'         -> HomePage
-// 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
+import { createTheme, ThemeProvider } from '@mui/material'
+import { purple, red } from '@mui/material/colors'
 
+import { useAuth } from '@redwoodjs/auth'
 import { Router, Route, Set } from '@redwoodjs/router'
 
 import MainLayout from 'src/layouts/MainLayout'
 
+const moderatorTheme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+  },
+})
+
+const adminTheme = createTheme({
+  palette: {
+    primary: {
+      main: red[500],
+    },
+  },
+})
+
+const defaultTheme = createTheme({})
+
+const getCustomTheme = (role: string) => {
+  if (role === 'moderator') return moderatorTheme
+  if (role === 'admin') return adminTheme
+  return defaultTheme
+}
+
 const Routes = () => {
+  const { loading, isAuthenticated, currentUser } = useAuth()
+  if (loading) return null
   return (
-    <Router>
-      <Route path="/login" page={LoginPage} name="login" />
-      <Route path="/signup" page={SignupPage} name="signup" />
-      <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
-      <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
-      <Set wrap={MainLayout}>
-        <Route path="/" page={HomePage} name="home" />
-      </Set>
-      <Route notfound page={NotFoundPage} />
-    </Router>
+    <ThemeProvider theme={isAuthenticated ? getCustomTheme(currentUser.roles as string) : defaultTheme}>
+      <Router>
+        <Route path="/login" page={LoginPage} name="login" />
+        <Route path="/signup" page={SignupPage} name="signup" />
+        <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
+        <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
+        <Set wrap={MainLayout}>
+          <Route path="/" page={HomePage} name="home" />
+        </Set>
+        <Route notfound page={NotFoundPage} />
+      </Router>
+    </ThemeProvider>
   )
 }
 
--- web/src/layouts/MainLayout/MainLayout.tsx
+++ web/src/layouts/MainLayout/MainLayout.tsx
@@ -1,9 +1,31 @@
+import MenuIcon from '@mui/icons-material/Menu'
+import { AppBar, Box, IconButton, Toolbar } from '@mui/material'
+
+import { useAuth } from '@redwoodjs/auth'
+
 type MainLayoutProps = {
   children?: React.ReactNode
 }
 
 const MainLayout = ({ children }: MainLayoutProps) => {
-  return <>{children}</>
+  return (
+    <Box>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      {children}
+    </Box>
+  )
 }
 
 export default MainLayout
