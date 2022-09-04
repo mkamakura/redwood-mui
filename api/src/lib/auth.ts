62aeebca829b3961b2dd9590039ac0889eec8588
mkamakura <kamakura.masaya@gmail.com>
2022-09-04 14:33:00 +0900

    add roles to user

--- api/src/lib/auth.ts
+++ api/src/lib/auth.ts
@@ -1,4 +1,5 @@
 import { AuthenticationError, ForbiddenError } from '@redwoodjs/graphql-server'
+
 import { db } from './db'
 
 /**
@@ -21,7 +22,7 @@ import { db } from './db'
 export const getCurrentUser = async (session) => {
   return await db.user.findUnique({
     where: { id: session.id },
-    select: { id: true },
+    select: { id: true, email: true, roles: true },
   })
 }
 
@@ -59,9 +60,6 @@ export const hasRole = (roles: AllowedRoles): boolean => {
     if (typeof currentUserRoles === 'string') {
       // roles to check is a string, currentUser.roles is a string
       return currentUserRoles === roles
-    } else if (Array.isArray(currentUserRoles)) {
-      // roles to check is a string, currentUser.roles is an array
-      return currentUserRoles?.some((allowedRole) => roles === allowedRole)
     }
   }
 
